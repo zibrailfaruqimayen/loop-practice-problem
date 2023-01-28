27cd5502b82b928c552d7c44013bd12066e636bb
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-01-28 17:22:27 +0600

    js-for

---
+++ for.js
@@ -0,0 +1,25 @@
+// var roastGiven = 0;
+// while(roastGiven < 7){
+//     console.log('Roast Den Please !!!')
+//     roastGiven++;
+// }
+
+
+// for loop
+
+for(var roastGiven = 0; roastGiven < 7; roastGiven++){
+    console.log('Roast Den Please !!!')
+}
+
+
+for(var i = 0; i < 7; i++){
+    console.log('i !!!')
+}
+
+for(var i = 1; i <= 10; i++){
+    console.log(i);
+}
+
+for(var i = 0; i <= 20; i+=2){
+    console.log(i);
+}
