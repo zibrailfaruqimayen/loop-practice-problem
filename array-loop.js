e63da87dfe29209cbe837970aea25f0cde064190
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-01-28 17:47:54 +0600

    js-array-loop

---
+++ array-loop.js
@@ -0,0 +1,24 @@
+/* for(var i = 0; i < 10; i++){
+    console.log(i);
+}
+ */
+
+// target: display every elements of an array
+
+// var numbers = [45, 87, 89, 56, 32, 51, 25];
+// for(var i = 0; i < 7; i++){
+//     var number = numbers[i];
+//     console.log(number);
+//  }
+
+//  var numbers = [45, 87, 89, 56, 32, 51, 25];
+// for(var i = 0; i < numbers.length; i++){
+//     var number = numbers[i];
+//     console.log(number);
+//  }
+
+var items = ['bottle', 'mouse', 'sunglass', 'pen', 'notebook']
+for(var  i = 0; i < items.length; i++){
+    var item = items[i];
+    console.log(item);
+}
